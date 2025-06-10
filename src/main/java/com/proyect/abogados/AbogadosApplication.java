621663cba6dce6773c4930b31c8496989a91cc62
iTzAsaro <alexdergamer2006@gmail.com>
2025-06-09 22:54:35 -0400

    Actualizo AbogadosApplication con verificaciones de programa funcionando

--- src/main/java/com/proyect/abogados/AbogadosApplication.java
+++ src/main/java/com/proyect/abogados/AbogadosApplication.java
@@ -7,7 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AbogadosApplication {
 
 	public static void main(String[] args) {
+		// Hook para mostrar mensaje al cerrar el programa
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			System.out.println("Cerrando programa... ¡Hasta pronto!");
+		}));
+
 		SpringApplication.run(AbogadosApplication.class, args);
+		System.out.println("Desplegando aplicación Abogados...");
+		System.out.println(" Aplicación Abogados iniciada correctamente.");
+		System.out.println("Ingresar a documentacion: http://localhost:8080/doc/swagger-ui/index.html");
 	}
 
 }
